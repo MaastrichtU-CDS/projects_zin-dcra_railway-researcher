2fc1ec3d5f6754e2691a264796af533d87e05c4f
Johan van Soest <johan.vansoest@maastro.nl>
2020-02-19 19:54:51 +0100

    Running example now in python

--- railway_researcher.py
+++ railway_researcher.py
@@ -1,15 +1,61 @@
+from datetime import datetime
+import json
+import requests
+import time
+
 class RailwayResearcher:
-    requests = __import__('requests')
-    json = __import__("json")
-    def __init__(self, url, user, password):
+    def __init__(self, url, user, password, urlRailway):
         self.apiUrl = url
         self.apiUser = user
         self.apiPass = password
         self.apiToken = None
+        self.railwayApi = urlRailway
         self.__connect()
     
     def __connect(self):
-        response = self.requests.post(self.apiUrl + "/auth/realms/railway/protocol/openid-connect/token",
+        response = requests.post(self.apiUrl + "/auth/realms/railway/protocol/openid-connect/token",
             data={"grant_type": "client_credentials"},
             auth=(self.apiUser, self.apiPass))
-        self.apiToken = self.json.loads(response.text)['access_token']
+        self.apiToken = json.loads(response.text)['access_token']
+    
+    def createTrain(self, name, dockerImageUrl, ownerId):
+        response = requests.post(self.railwayApi + "/api/trains?access_token=" + self.apiToken,
+            json={
+                "name": name,
+                "dockerImageUrl": dockerImageUrl,
+                "ownerId": ownerId,
+                "calculationStatus":"REQUESTED", 
+                "currentIteration":0
+            },
+            headers={
+                "Content-Type": "application/json",
+            })
+        return response.json()
+    
+    def createTask(self, trainStruct, calculationStatus, result, stationId, iteration, currentIteration, master, input):
+        response = requests.post(self.railwayApi + "/api/trains/"+str(trainStruct['id'])+"/tasks?access_token=" + self.apiToken,
+            json={
+                "creationTimestamp": datetime.now().isoformat(),
+                "calculationStatus": calculationStatus,
+                "result": result,
+                "stationId": stationId,
+                "iteration": iteration,
+                "currentIteration": currentIteration,
+                "master": master,
+                "input": input
+            },
+            headers={
+                "Content-Type": "application/json",
+            })
+        return response.json()
+    
+    def getTaskResult(self, taskStruct, waitCompleted=False):
+        url = self.railwayApi + "/api/tasks/" + str(taskStruct['id']) + "?access_token=" + self.apiToken
+        
+        responseJson = requests.get(url).json()
+        
+        while (responseJson["calculationStatus"] in ["REQUESTED", "PROCESSING"]):
+            time.sleep(5)
+            responseJson = requests.get(url).json()
+        
+        return responseJson
--- run.py
+++ run.py
@@ -1,3 +1,29 @@
 from railway_researcher import RailwayResearcher
+import json
+import time
 
-railway = RailwayResearcher("https://dcra-keycloak.railway.medicaldataworks.nl", "johan", "c9a295ff-bdde-47de-b82c-88ae5e38736d")
+keycloakUrl = "https://dcra-keycloak.railway.medicaldataworks.nl"
+keycloakClient = "johan"
+keycloakToken = "c9a295ff-bdde-47de-b82c-88ae5e38736d"
+railwayUrl = "https://dcra.railway.medicaldataworks.nl"
+
+railway = RailwayResearcher(keycloakUrl, keycloakClient, keycloakToken, railwayUrl)
+train = railway.createTrain("statistics", "registry.gitlab.com/medicaldataworks/railway/prototypetrain:master", "03838bb4-8103-4a98-a9c3-d4848b13b3f6")
+print("====================Train====================")
+print(train)
+
+task1 = railway.createTask(train, "COMPLETED", json.dumps({"calculation_result": 6}), 2, 0, 0, False, "")
+print("====================Task 1====================")
+print(task1)
+
+task2 = railway.createTask(train, "COMPLETED", json.dumps({"calculation_result": 10}), 3, 0, 0, False, "")
+print("====================Task 2====================")
+print(task2)
+
+task3 = railway.createTask(train, "REQUESTED", "", 3, 0, 0, True, json.dumps({"iterations": 0}))
+print("====================Task 3====================")
+print(task3)
+
+taskResult = railway.getTaskResult(task3, waitCompleted=True)
+print("====================Result====================")
+print(taskResult)
