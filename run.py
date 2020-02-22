dea645bc297f06aa4d3061350c543af4c57c4ad2
Johan van Soest <johan.vansoest@maastro.nl>
2020-02-22 13:16:38 +0100

    Now using calculation container for current project

--- run.py
+++ run.py
@@ -4,26 +4,18 @@ import time
 
 keycloakUrl = "https://dcra-keycloak.railway.medicaldataworks.nl"
 keycloakClient = "johan"
-keycloakToken = "c9a295ff-bdde-47de-b82c-88ae5e38736d"
+keycloakToken = "b7940067-f9a7-41fb-b74b-f672fcdbfb6e"
 railwayUrl = "https://dcra.railway.medicaldataworks.nl"
 
 railway = RailwayResearcher(keycloakUrl, keycloakClient, keycloakToken, railwayUrl)
-train = railway.createTrain("statistics", "registry.gitlab.com/medicaldataworks/railway/prototypetrain:master", "03838bb4-8103-4a98-a9c3-d4848b13b3f6", 2)
+train = railway.createTrain("statistics", "registry.gitlab.com/um-cds/projects/zin-dcra/prototypetrain:master", "03838bb4-8103-4a98-a9c3-d4848b13b3f6", 2)
 print("====================Train====================")
 print(train)
 
-task1 = railway.createTask(train, "COMPLETED", json.dumps({"calculation_result": 6}), "timh", 0, 0, False, "")
+task1 = railway.createTask(train, "REQUESTED", "", "maastro", 0, 0, False, "")
 print("====================Task 1====================")
 print(task1)
 
-task2 = railway.createTask(train, "COMPLETED", json.dumps({"calculation_result": 10}), "johan", 0, 0, False, "")
-print("====================Task 2====================")
-print(task2)
-
-task3 = railway.createTask(train, "REQUESTED", "", "maastro", 0, 0, True, json.dumps({"iterations": 3}))
-print("====================Task 3====================")
-print(task3)
-
-taskResult = railway.getTaskResult(task3, waitCompleted=True)
+taskResult = railway.getTaskResult(task1, waitCompleted=True)
 print("====================Result====================")
 print(taskResult)
